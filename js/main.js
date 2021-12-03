a9d5d79b9c74a6f5c52e6793497d53d49afded3e
Yurii <rhodenskilled@gmail.com>
2021-12-03 15:47:49 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+"use strict";
+
+
+// lesson Переменные и строгий режим
+
+a = 15;
+
+console.log(a);
+
+
+let number = 5;
+
+const leftBorderWidth=1;
+
+number = 10;
+console.log(number);
+
+// leftBorderWidth=10;
+
+// const obj={
+//   a:50
+// };
+// obj.a=10;
+// console.log(obj);
+
+
+let sign =4.6;
+console.log(-4/0);
+
+console.log('string'*6);
+
+ 
+const obj = {
+  name:"john",
+  age:25,
+  isMaried:false
+};
+
+// console.log(obj.name)
+console.log(obj['age']);
+
+
+
+let arr = ['plum.png', 'orange,jpg', 6, {}, 'apple.bmp', []];
+
+console.log(arr[4]);
+
+// {var result=50;}
+
+// console.log(result);
+
---
+++ js/script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+// lesson общение с пользователем
+
+// alert('hello!');
+
+// const result=confirm("are you there?");
+// console.log(result);
+
+// const answer=+prompt("are you age's 18+ ? , 18");
+// console.log(answer+5);
+
+
+
+// const answers=[];    
+
+// answers[0]=prompt('как ваше имя?','');
+// answers[1]=prompt('как ваша фамилия?','');
+// answers[2]=prompt('сколько вам лет?','');
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+
+
+// lesson Итерполяция
+
+// const category='toys';
+
+// console.log(`https://someurl.com/${category}/66`);
+
+// const user ='ivan';
+// alert(`hello, ${user}`);
+
+
+
+
+
+// lesson операторы в js
+
+// console.log('arr'+ '-object');
+
+// console.log(4 + +'object');
+
+// console.log(4+  "5");
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+    // incr++;
+    // decr--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+    console.log(2*4==='8');
+
+
+    // && и
+  //  ||  или
+
+  // ! Отрицание 
+
+
+    
+
+    const isChecked=true,
+          isClose=true;
+
+          console.log(isChecked && isClose);
+
+
+          // const isChecked=false,
+          // isClose=false;
+
+          // console.log(isChecked || !isClose);
+
+
+
+
+ 
+
+
+
+
+
